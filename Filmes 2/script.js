87f00df15fbd5848e3019d888ebc88189a5dcf31
Emerson-spec <emersonbispho@gmail.com>
2024-09-07 22:08:11 -0300

    Add files via upload

---
+++ Filmes 2/script.js
@@ -0,0 +1,27 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Menu de Hambúrguer
+    const menuToggle = document.getElementById('menu-toggle');
+    const menu = document.getElementById('menu');
+
+    menuToggle.addEventListener('click', function() {
+        menu.classList.toggle('hidden');
+    });
+
+    // Navegação suave
+    const links = document.querySelectorAll('a[href^="#"]');
+
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
